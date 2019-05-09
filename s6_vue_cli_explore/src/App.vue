bb7d7be2a6be8bfeff0f9ae5a6b31be69a305567
Logan Wood <lwood3499@gmail.com>
2019-05-08 21:01:10 -0500

    vue component structure and style scoping

--- s6_vue_cli_explore/src/App.vue
+++ s6_vue_cli_explore/src/App.vue
@@ -2,28 +2,24 @@
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png">
             <h1>Hello World!</h1>
-		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+            <app-servers/>
 	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-	name: "app",
-	components: {
-		HelloWorld
-	}
-};
 </script>
 
 <style lang="scss">
-#app {
-	font-family: "Avenir", Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
+      #app {
+            font-family: "Avenir", Helvetica, Arial, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-align: center;
+            color: #2c3e50;
+            margin-top: 60px;
+      }
+      div {
+            border: 1px solid red;
+      }
 </style>
---
+++ s6_vue_cli_explore/src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+      <div>
+            <app-server-status v-for= "(server, i) in 5" :key="i"></app-server-status>
+      </div>
+</template>
+
+<script>
+      import ServerStatus from './ServerStatus.vue';
+      export default {
+            components: {
+                  'app-server-status': ServerStatus
+            }
+      }
+</script>
+
+<style scoped>
+      div {
+            border: 1px solid blue;
+      }
+</style>
