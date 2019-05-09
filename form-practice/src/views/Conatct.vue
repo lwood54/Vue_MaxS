b63dd926bd48a4632985a3d8194bba6312aa7504
Logan Wood <lwood3499@gmail.com>
2019-05-08 22:46:13 -0500

    practicing with forms

---
+++ form-practice/src/views/Conatct.vue
@@ -0,0 +1,22 @@
+<template>
+      <div class="contact">
+            <h1>Conact Page</h1>
+            <h2>Logan Wood</h2>
+      </div>
+</template>
+
+<script>
+      export default {
+            data: function() {
+                  return {
+                        greeting: "Hello World!"
+                  }
+            }
+      }
+</script>
+
+<style lang="scss" scoped>
+      h1 {
+            color: blue;
+      }
+</style>
