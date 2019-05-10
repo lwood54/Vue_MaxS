e31cc19432c4ae32cda1a55d7cb49801429ca295
Logan Wood <lwood3499@gmail.com>
2019-05-09 22:54:28 -0500

    sending parent to child and child to parent with ()

---
+++ s8_code/src/components/UserDetail.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>User Name: {{name}}</p>
+        <button @click="reverseString">Reverse Name</button>
+        <p >Reversed name: {{reverseName}}</p>
+        <hr>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+
+      export default {
+            props: {
+                  name: {
+                        type: String,
+                        required: true
+                  },
+                  handleAlert: Function
+            },
+            data: function() {
+                  return {
+                        reverseName: this.name
+                  }
+            },
+            methods: {
+                  reverseString() {
+                        this.reverseName = this.name.split("").reverse().join("");
+                  },
+                  resetName() {
+                        this.name = "Logan";
+                        this.$emit("nameReset", this.name);
+                  }
+            }
+          }
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
