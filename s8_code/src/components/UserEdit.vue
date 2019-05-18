be0f1939aa43befae224bd1cf2780dcdcb49d6cb
Logan Wood <lwood3499@gmail.com>
2019-05-17 21:06:36 -0500

    component methods

--- s8_code/src/components/UserEdit.vue
+++ s8_code/src/components/UserEdit.vue
@@ -1,15 +1,23 @@
 <template>
-    <div class="component">
-        <h3>You may edit the User here</h3>
-        <p>Edit me!</p>
-    </div>
+	<div class="component">
+		<h3>You may edit the User here</h3>
+		<p>Edit me!</p>
+		<button @click="editAge">Edit Age</button>
+	</div>
 </template>
 
 <script>
+	export default {
+		methods: {
+			editAge: function() {
+				console.log("age edited");
+			}
+		}
+	};
 </script>
 
 <style scoped>
-    div {
-        background-color: lightgreen;
-    }
+	div {
+		background-color: lightgreen;
+	}
 </style>
