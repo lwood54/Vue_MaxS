51ec291fd0d271b36c339b9462b98fa49c8b8c4b
Logan Wood <lwood3499@gmail.com>
2019-05-19 09:03:39 -0500

    completed s9_challenge

--- s9_challenge/src/App.vue
+++ s9_challenge/src/App.vue
@@ -1,32 +1,53 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
-                <hr>
-                <app-blue></app-blue>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<br>
+				<button class="btn btn-primary" @click="handleColorSelect('blue')">Load Blue Template</button>
+				<button class="btn btn-success" @click="handleColorSelect('green')">Load Green Template</button>
+				<button class="btn btn-danger" @click="handleColorSelect('red')">Load Red Template</button>
+				<hr>
+				<component :is="selectedColor">Here are some words.</component>
+				<!-- <app-blue></app-blue>
                 <app-green></app-green>
-                <app-red></app-red>
-            </div>
-        </div>
-    </div>
+				<app-red></app-red>-->
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+	import Blue from "./components/Blue.vue";
+	import Green from "./components/Green.vue";
+	import Red from "./components/Red.vue";
 
-    export default {
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
-        }
-    }
+	export default {
+		data() {
+			return {
+				selectedColor: "app-blue"
+			};
+		},
+		methods: {
+			handleColorSelect(color) {
+				switch (color) {
+					case "blue":
+						this.selectedColor = "app-blue";
+						break;
+					case "green":
+						this.selectedColor = "app-green";
+						break;
+					case "red":
+						this.selectedColor = "app-red";
+						break;
+				}
+			}
+		},
+		components: {
+			appBlue: Blue,
+			appGreen: Green,
+			appRed: Red
+		}
+	};
 </script>
 
 <style>
