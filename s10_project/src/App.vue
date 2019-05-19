25fb47a72267a182738a5a75dd0210dec8467e25
Logan Wood <lwood3499@gmail.com>
2019-05-19 15:42:27 -0500

    s10 project init

---
+++ s10_project/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="app">
+		<h1>s10 Project</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "app",
+		components: {}
+	};
+</script>
+
+<style lang="scss">
+</style>
