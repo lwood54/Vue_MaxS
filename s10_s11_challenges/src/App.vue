a16a314047a4fc8da372474f3a2a699e70f12aaf
Logan Wood <lwood3499@gmail.com>
2019-05-20 22:21:35 -0500

    completed s11 form challenge

--- s10_s11_challenges/src/App.vue
+++ s10_s11_challenges/src/App.vue
@@ -11,6 +11,40 @@
 				<div class="alert alert-info">Info: Click on a Quote to delete it!</div>
 			</div>
 		</div>
+		<hr>
+		<hr>
+		<!-- Exercise 1 -->
+		<!-- Create a Signup Form where you retrieve the following Information -->
+		<!-- Full Name (First Name + Last Name) -->
+		<!-- Mail -->
+		<!-- Password -->
+		<!-- Store Data? Yes/No -->
+
+		<user-form v-if="!isSubmitted" @userData="handleUserData"/>
+
+		<!-- Exercise 2 -->
+		<!-- Only display the Form if it has NOT been submitted -->
+		<!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+		<!-- Exercise 3 -->
+		<!-- Edit the Example from above and create a custom "Full Name" Control -->
+		<!-- which still holds the First Name and Last Name Input Field -->
+		<div class="row" v-else>
+			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>Your Data</h4>
+					</div>
+					<div class="panel-body">
+						<p>Full Name: {{userData.firstName}} {{userData.lastName}}</p>
+						<p>Mail: {{userData.email}}</p>
+						<p>Password: {{userData.password}}</p>
+						<p>Store in Database?: {{userData.storeDataResponse}}</p>
+						<button @click="isSubmitted = false">clear form</button>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -18,13 +52,16 @@
 	import QuoteGrid from "./components/QuoteGrid";
 	import NewQuote from "./components/NewQuote";
 	import Header from "./components/Header";
+	import UserForm from "./components/UserForm";
 
 	export default {
 		data() {
 			return {
 				quotes: ["A quote to get started..."],
 				maxQuotes: 10,
-				errorMessage: ""
+				errorMessage: "",
+				userData: {},
+				isSubmitted: false
 			};
 		},
 		methods: {
@@ -46,12 +83,17 @@
 				// when handling larger arrays?
 				this.errorMessage = "";
 				this.quotes.splice(val, 1);
+			},
+			handleUserData(val) {
+				this.userData = val.userData;
+				this.isSubmitted = val.isSubmitted;
 			}
 		},
 		components: {
 			"quote-grid": QuoteGrid,
 			"new-quote": NewQuote,
-			"progress-header": Header
+			"progress-header": Header,
+			"user-form": UserForm
 		}
 	};
 </script>
---
+++ s10_s11_challenges/src/components/UserForm.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<!-- <form @input="handleChange"> -->
+		<form>
+			<label for="firstName">
+				First Name
+				<input type="text" v-model="userData.firstName">
+			</label>
+			<label for="lastName">
+				Last Name
+				<input type="text" v-model="userData.lastName">
+			</label>
+			<label for="email">
+				Email
+				<input type="email" v-model="userData.email">
+			</label>
+			<label for="password">
+				Password
+				<input type="password" v-model="userData.password">
+			</label>
+			<h3>Store Data? {{userData.storeDataResponse}}</h3>
+			<input type="radio" id="yes" value="yes" v-model="userData.storeDataResponse">
+			<label for="yes">Yes</label>
+			<br>
+			<input type="radio" id="no" value="no" v-model="userData.storeDataResponse">
+			<label for="no">No</label>
+			<button @click.prevent="handleSubmit">Submit</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				userData: {
+					firstName: "",
+					lastName: "",
+					email: "",
+					password: "",
+					storeDataResponse: "yes"
+				}
+			};
+		},
+		methods: {
+			handleChange() {
+				this.$emit("userData", this.userData);
+			},
+			handleSubmit() {
+				this.$emit("userData", {
+					userData: this.userData,
+					isSubmitted: true
+				});
+			}
+		}
+	};
+</script>
+
+<style>
+</style>
