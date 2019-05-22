444349817f8612a0bd1d2af43869f82832acb1af
Logan Wood <lwood3499@gmail.com>
2019-05-21 21:16:37 -0500

    creating custon directives registered locally

--- s10_s11_challenges/src/components/DirectivesPractice.vue
+++ s10_s11_challenges/src/components/DirectivesPractice.vue
@@ -6,11 +6,79 @@
 		<hr>
 		<h1>Custon Directives</h1>
 		<p v-highlight:background.delayed="'red'">Color this</p>
+		<p
+			v-local-highlight:background.delayed.blink="{
+                  mainColor: 'red',
+                  secondColor: 'green',
+                  delay: 500
+            }"
+		>Color this, local highlight!</p>
+		<hr>
+		<h1>Build a Custon Directive Challenge</h1>
+		<h2 v-if="!showReplacement" v-my-on:click="handleClick">Click me to disappear!</h2>
+		<h4 v-if="showReplacement" v-my-on:click="handleClick">my smaller replacement shows up now...</h4>
 	</div>
 </template>
 
 <script>
-	export default {};
+	export default {
+		data() {
+			return {
+				showReplacement: false
+			};
+		},
+		methods: {
+			handleClick(el) {
+				this.showReplacement = !this.showReplacement;
+			}
+		},
+		directives: {
+			"local-highlight": {
+				bind(el, binding, vnode) {
+					let delay = 0;
+					if (binding.modifiers["delayed"]) {
+						delay = 3000;
+					}
+					if (binding.modifiers["blink"]) {
+						let mainColor = binding.value.mainColor;
+						let secondColor = binding.value.secondColor;
+						let currentColor = mainColor;
+						setTimeout(() => {
+							setInterval(() => {
+								currentColor === secondColor
+									? (currentColor = mainColor)
+									: (currentColor = secondColor);
+								if (binding.arg == "background") {
+									el.style.backgroundColor = currentColor;
+								} else {
+									el.style.color = currentColor;
+								}
+							}, binding.value.delay);
+						}, delay);
+					} else {
+						setTimeout(() => {
+							if (binding.arg == "background") {
+								el.style.backgroundColor =
+									binding.value.mainColor;
+							} else {
+								el.style.color =
+									binding.value.mainColor;
+							}
+						}, delay);
+					}
+				}
+			},
+			"my-on": {
+				bind(el, binding, vnode) {
+					if (binding.arg === "click") {
+						el.addEventListener("click", () => {
+							binding.value(el);
+						});
+					}
+				}
+			}
+		}
+	};
 </script>
 
 <style>
