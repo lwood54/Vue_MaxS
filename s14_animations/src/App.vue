71d76e58f4c1bd53c88ba344b030d9208f1edf55
Logan Wood <lwood3499@gmail.com>
2019-05-23 20:27:41 -0500

    basics with transitions

---
+++ s14_animations/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>S 14 - Animations</h1>
+		<animations-1></animations-1>
+	</div>
+</template>
+
+<script>
+	import Animations1 from "./components/Animations1";
+
+	export default {
+		components: {
+			"animations-1": Animations1
+		}
+	};
+</script>
+
+<style lang="scss">
+</style>
