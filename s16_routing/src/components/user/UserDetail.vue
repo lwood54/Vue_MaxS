70ae33a4f8747813add385f9f1e11b61653a9b62
Logan Wood <lwood3499@gmail.com>
2019-05-30 18:37:59 -0500

    page location with hash and scrollBehavior

--- s16_routing/src/components/user/UserDetail.vue
+++ s16_routing/src/components/user/UserDetail.vue
@@ -6,10 +6,21 @@
 		<!-- Both of these are basically saying: "use the id received from going to this user page and
 		pass it to the route"-->
 		<!-- <router-link tag="button" :to="`/user/${$route.params.id}/edit`" class="btn btn-primary">Edit User</router-link> -->
-		<router-link
-			tag="button"
-			:to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
-			class="btn btn-primary"
-		>Edit User</router-link>
+		<router-link tag="button" :to="link" class="btn btn-primary">Edit User</router-link>
 	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				link: {
+					name: "userEdit",
+					params: { id: this.$route.params.id }, // change to this.$route because relocation to vue instance
+					query: { locale: "en", q: 100 },
+					hash: "#data"
+				}
+			};
+		}
+	};
+</script>
