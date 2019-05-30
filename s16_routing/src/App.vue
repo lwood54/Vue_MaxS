f88db380fd713e3e6eccd61a7393fd2bc90547d0
Logan Wood <lwood3499@gmail.com>
2019-05-29 20:50:41 -0500

    setting up router section

---
+++ s16_routing/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<div class="content-container">
+			<h1>Routing</h1>
+			<hr>
+			<router-view></router-view>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style lang="scss">
+	.content-container {
+		width: 50%;
+		margin: 10px auto;
+	}
+</style>
