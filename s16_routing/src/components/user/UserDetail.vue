93437c6b16f52bb86a4403fecaad96fc59576135
Logan Wood <lwood3499@gmail.com>
2019-05-30 17:46:20 -0500

    using query parameters

--- s16_routing/src/components/user/UserDetail.vue
+++ s16_routing/src/components/user/UserDetail.vue
@@ -8,7 +8,7 @@
 		<!-- <router-link tag="button" :to="`/user/${$route.params.id}/edit`" class="btn btn-primary">Edit User</router-link> -->
 		<router-link
 			tag="button"
-			:to="{name: 'userEdit', params: {id: $route.params.id}}"
+			:to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
 			class="btn btn-primary"
 		>Edit User</router-link>
 	</div>
