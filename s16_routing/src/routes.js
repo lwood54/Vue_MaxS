989ddb667c528d1bac1d0d9b5cb205a145d6d78a
Logan Wood <lwood3499@gmail.com>
2019-05-30 19:13:28 -0500

    basic component lazy loading with webpack

--- s16_routing/src/routes.js
+++ s16_routing/src/routes.js
@@ -1,9 +1,28 @@
-import User from "./components/user/User";
-import UserStart from "./components/user/UserStart";
-import UserDetail from "./components/user/UserDetail";
-import UserEdit from "./components/user/UserEdit";
 import Home from "./components/Home";
 import Header from "./components/Header";
+
+// lazy loading with webpack
+const User = resolve => {
+      require.ensure(['./components/user/User.vue'], () => {
+            resolve(require('./components/user/User.vue'));
+      });
+}
+const UserStart = resolve => {
+      require.ensure(['./components/user/UserStart.vue'], () => {
+            resolve(require('./components/user/UserStart.vue'));
+      });
+}
+const UserEdit = resolve => {
+      require.ensure(['./components/user/UserEdit.vue'], () => {
+            resolve(require('./components/user/UserEdit.vue'));
+      });
+}
+const UserDetail = resolve => {
+      require.ensure(['./components/user/UserDetail.vue'], () => {
+            resolve(require('./components/user/UserDetail.vue'));
+      });
+}
+
 
 export const routes = [
       { path: '', /*component: Home,*/ name: 'home', components: {
