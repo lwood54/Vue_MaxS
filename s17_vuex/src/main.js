6411701417ae8a2f5568aab4ee9ac15de1a4aa34
Logan Wood <lwood3499@gmail.com>
2019-06-01 14:36:57 -0500

    setting up very basic Vuex store

--- s17_vuex/src/main.js
+++ s17_vuex/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { store } from "./store/store";
+
 new Vue({
   el: '#app',
+  store, // register the store like you would do with the router
   render: h => h(App)
 })
