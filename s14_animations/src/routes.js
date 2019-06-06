69ee6dcb33f3b6e186b0dfcbaf7f5043b0b2fc1f
Logan Wood <lwood3499@gmail.com>
2019-06-05 22:25:51 -0500

    updated routes.js

--- s14_animations/src/routes.js
+++ s14_animations/src/routes.js
@@ -1,10 +1,9 @@
-import Animations1 from "./components/Animations1";
-import GameBoard from "./components/GameBoard";
+import QuizApp from "./components/QuizProject/QuizApp.vue";
+import CitiesGame from "./components/CitiesGame";
 import App from "./App";
 
 // not currently being used, because I wanted to combine into one file for now.
 export const routes = [
-            { path: '/', component: App },
-            { path: '/animations', component: Animations1 },
-            { path: '/GameBoard', component: GameBoard }
+      { path: '/', component: QuizApp },
+      { path: '/cities', component: CitiesGame },
 ]
